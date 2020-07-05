c03c1288b556e9b512a410f48529d847546f5177
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-07-05 08:59:43 +0100

    UPDATED - Created element create function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const PAGE_TOKEN ="EAACNbZCLb8Y0BAFbmmnPqnV8aDG7LgcIYY96P14H6eUuG6FruZB7bO6Q0zTrYApfbaRJf5DLN1VPmvENa4IwLmfBU5ZBKJzRZA65DdUFdzRnDco6vftMezij2NY2EkyFX0zcHlXiZCb47RqaLCurT16emH7AErJWHDO7ydTMhJpEJQJKiLnZAk";
+const PAGE_TOKEN = "EAACNbZCLb8Y0BAFbmmnPqnV8aDG7LgcIYY96P14H6eUuG6FruZB7bO6Q0zTrYApfbaRJf5DLN1VPmvENa4IwLmfBU5ZBKJzRZA65DdUFdzRnDco6vftMezij2NY2EkyFX0zcHlXiZCb47RqaLCurT16emH7AErJWHDO7ydTMhJpEJQJKiLnZAk";
 
 let fbMediaNodeUrl = `https://graph.facebook.com/v7.0/17841408168194339?fields=media&access_token=${PAGE_TOKEN}`
 let fbMediaDataUrl = `https://graph.facebook.com/v7.0/{{media}}?fields=children%7Bmedia_url%7D,media_url,timestamp,id,username,media_type,caption,like_count,comments_count&access_token=${PAGE_TOKEN}`
@@ -33,10 +33,10 @@ async function getMediaData() {
   for (const image of usersImages) {
     if (image.media_type === "VIDEO") {
       addVideoToScreen(image)
-    } else if (image.media_type === "CAROUSEL_ALBUM") {
+    } else if (image.media_type === "CAROUSEL_ALBUM") {      
       addCarouselImageToScreen(image)
     } else {
-      addImageToScreen(image);    
+      addImageToScreen(image);
     }
   }
 }
@@ -48,113 +48,194 @@ async function storeMediaData(url) {
 }
 
 function addImageToScreen(image) {
-  let imageDiv = document.createElement('div')
-  let imageElement = new Image()
-  let caption = addCaption(image.caption, image.like_count, image.comments_count);
-  imageDiv.setAttribute('class', 'imageDiv')
-  imageElement.src = image.media_url
-  imageElement.setAttribute('class', 'image')
-  imageDiv.appendChild(imageElement);
-  imageDiv.appendChild(caption);
-  imageDiv.setAttribute('id', image.id);
-  app.appendChild(imageDiv);
+  let imageOptions = {
+    type: 'div',
+    attributes: {
+      id: image.id,
+      class: 'imageDiv'
+    },
+    children: [
+      createMediaEl('img', 'image', image.media_url),
+      createCaption(image.caption, image.like_count, image.comments_count)
+    ]
+  }
+  app.appendChild(createDomElements(imageOptions));
 }
 
-function addCarouselImageToScreen(image) {  
+function addCarouselImageToScreen(image) {    
   let slideArrayId = slideIndex.length;
   let carouselClass = image.id;
-  let caption = addCaption(image.caption, image.like_count, image.comments_count);
+  let carouselOptions = {
+    type: 'div',
+    attributes: {
+      id: image.id,
+      class: 'carousel'
+    },
+    children: [
+      ...createImageArray(image.children.data, carouselClass),
+      createSlideDiv(slideArrayId),
+      createCaption(image.caption, image.like_count, image.comments_count)
+    ]
+  }  
   slideId.push(carouselClass);
-  slideIndex.push(1);  
-  let div = document.createElement('div');
-  div.setAttribute('class', 'carousel')
-  image.children.data.forEach(child => {
-    let imageElement = new Image()
-    imageElement.src = child.media_url
-    imageElement.setAttribute('class', 'child ' + carouselClass)
-    div.appendChild(imageElement);
-  })
-  let innerDiv = document.createElement('div')
-  let prev = document.createElement('a');
-  let next = document.createElement('a');
-  prev.setAttribute('class', 'prev');
-  next.setAttribute('class', 'next');
-  prev.setAttribute("onclick", `plusSlides(-1, ${slideArrayId})`);
-  next.setAttribute("onclick", `plusSlides(1, ${slideArrayId})`);
-  prev.innerHTML = '&#10094;';
-  next.innerHTML = '&#10095;';
-  innerDiv.appendChild(prev);
-  innerDiv.appendChild(next);
-  div.appendChild(innerDiv);
-  div.appendChild(caption);
-  div.setAttribute('id', image.id);
-  app.appendChild(div);  
+  slideIndex.push(1);
   showSlides(1, slideArrayId);
+  app.appendChild(createDomElements(carouselOptions));
+}
+
+function addVideoToScreen(image) {
+  let videoOptions = {
+    type: 'div',
+    attributes: {
+      id: image.id,
+      class: 'videoDiv'
+    },
+    children: [
+      createMediaEl('video', '', image.media_url),
+      createCaption(image.caption, image.like_count, image.comments_count)
+    ]
+  }
+  app.appendChild(createDomElements(videoOptions));
+}
+
+getMediaNodes()
+
+// Helper Functions
+function createDomElements(options) {
+  console.log(options);
+  let element = document.createElement(options.type)
+  for (const attr in options.attributes) {
+    if (attr === 'text') {
+      element.innerHTML = options.attributes[attr];
+    } else {
+      element.setAttribute(attr, options.attributes[attr])
+    }
+  }
+  if (options.children) {
+    options.children.forEach(child => {
+      element.appendChild(createDomElements(child))
+    });
+  }
+  return element;
+}
+
+function createMediaEl(type, classStr, imageUrl) {
+  return {
+    type: type,
+    attributes: {
+      class: classStr,
+      src: imageUrl,
+      controls: type == 'video' ? true : false
+    }
+  }
+}
+
+function createImageArray(images, carouselClass) {
+  let imagesArray = [];
+  images.forEach(child => {
+    imagesArray.push(createMediaEl('img', 'child ' + carouselClass, child.media_url));
+  })
+  return imagesArray;
+}
+
+function createCaption(caption, likes, comments) {
+  let captionDiv = {
+    type: 'div',
+    attributes: {
+      class: 'captionDiv',
+    },
+    children: [{
+      type: 'div',
+      attributes: {
+        class: 'caption'
+      },
+      children: [{
+        type: 'p',
+        attributes: {
+          text: caption
+        },
+        children: [{
+          type: 'div',
+          attributes: {
+            class: 'stats'
+          },
+          children: [{
+              type: 'span',
+              attributes: {
+                text: `${likes} &#x1F49B`
+              }
+            },
+            {
+              type: 'span',
+              attributes: {
+                text: `${comments} &#x1F4AC`
+              }
+            }
+          ]
+        }]
+      }]
+    }]
+  }
+  return captionDiv;
+}
+
+function createSlideDiv(slideid) {
+  return {
+    type: 'div',
+    attributes: {},
+    children: [
+      {
+        type: 'a',
+        attributes: {
+          class: 'prev',
+          onclick: `plusSlides(-1, ${slideid})`,
+          text: '&#10094;'
+        }
+      },
+      {
+        type: 'a',
+        attributes: {
+          class: 'next',
+          onclick: `plusSlides(1, ${slideid})`,
+          text: '&#10095;'
+        }
+      }
+    ]
+  }
 }
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n, no) {    
+function showSlides(n, no) {
   let i;
-  var x = document.getElementsByClassName(slideId[no]);  
-  if (n > x.length) {slideIndex[no] = 1}  
-  if (n < 1) {slideIndex[no] = x.length}  
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length
+  }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-  }    
-  x[slideIndex[no]-1].style.display = "block";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
 }
-
-function addVideoToScreen(image) {
-  let videoDiv = document.createElement('div');
-  let videoElement = document.createElement('video');
-  let caption = addCaption(image.caption, image.like_count, image.comments_count);
-  videoElement.src = image.media_url;
-  videoElement.controls = true;
-  videoDiv.appendChild(videoElement);
-  videoDiv.appendChild(caption);
-  videoDiv.setAttribute('class', 'videoDiv')
-  videoDiv.setAttribute('id', image.id);
-  app.appendChild(videoDiv);  
-}
-
-function addCaption(caption, likes, comments) {
-  let captionDiv = document.createElement('div');
-  let captionInnerDiv = document.createElement('div');
-  let statsDiv = document.createElement('div');
-  let paragraph = document.createElement('p');
-  let likesEl = document.createElement('span')
-  let commentsEl = document.createElement('span')
-  captionDiv.setAttribute('class', 'captionDiv')
-  captionInnerDiv.setAttribute('class', 'caption')
-  statsDiv.setAttribute('class', 'stats')
-  paragraph.innerHTML = caption;
-  likesEl.innerHTML = `${likes} &#x1F49B`;
-  commentsEl.innerHTML = `${comments} &#x1F4AC`;
-  statsDiv.append(commentsEl, likesEl)
-  captionInnerDiv.append(paragraph, statsDiv);
-  captionDiv.appendChild(captionInnerDiv) 
-  return captionDiv;
-}
-
-getMediaNodes()
-
 
 // User Access Token
-window.fbAsyncInit = function() {
-  FB.init({
-    appId            : '155511955386765',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v7.0'
-  });
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      var accessToken = response.authResponse.accessToken;
-      console.log(accessToken);
-      
-    } 
-  });
-};
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId            : '155511955386765',
+//     autoLogAppEvents : true,
+//     xfbml            : true,
+//     version          : 'v7.0'
+//   });
+//   FB.getLoginStatus(function(response) {
+//     if (response.status === 'connected') {
+//       var accessToken = response.authResponse.accessToken;
+//       console.log(accessToken);
+
+//     } 
+//   });
+// };
